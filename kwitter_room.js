1cd075a9228748dbf78f7783a43f16ecdb9f461c
5YASH <89920126+5YASH@users.noreply.github.com>
2022-01-05 18:46:06 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB902gLXmgsYeT8fErlYaP0XqobPp2FGJY",
+    authDomain: "kwitter-389c6.firebaseapp.com",
+    databaseURL: "https://kwitter-389c6-default-rtdb.firebaseio.com",
+    projectId: "kwitter-389c6",
+    storageBucket: "kwitter-389c6.appspot.com",
+    messagingSenderId: "213296795999",
+    appId: "1:213296795999:web:9d130db09b54e97c5066ea"
+  };
+  firebase.initializeApp(firebaseConfig);
